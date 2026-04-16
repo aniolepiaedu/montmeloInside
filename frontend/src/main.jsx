24f49272918738e03e2c629e61c2ddd4c350de33
Aniol Rodríguez <aniolrodriguez2005@gmail.com>
2026-04-16 16:16:22 +0200

    Carpetes login regist - mini estructura main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,21 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
+import { login } from "./pages/login/login.jsx";
+import { regist} from "./pages/regist/regist.jsx";
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Navigate to="/login" replace />} />
+        <Route path="/login" element={<login />} />
+        <Route path="/regist" element={<regist />} />
+        <Route path="*" element={<h1>404</h1>} />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )
