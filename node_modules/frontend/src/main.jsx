007f7b4eaeacbb27a3e349f72cdbc59938fe8157
AGM <arnau.garciam@mataro.epiaedu.cat>
2026-04-16 17:46:33 +0200

    Afegides noves pantalles

--- node_modules/frontend/src/main.jsx
+++ node_modules/frontend/src/main.jsx
@@ -1,21 +1,25 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./index.css";
 
-import App from './App.jsx'
-import Login from "./pages/login/login.jsx"
-import Regist from "./pages/regist/regist.jsx"
+import App from "./App.jsx";
+import Login from "./pages/login/login.jsx";
+import Regist from "./pages/regist/regist.jsx";
+import Destinacio from "./pages/destinacio/destinacio.jsx";
+import Perfil from "./pages/perfil/perfil.jsx";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/login" element={<Login />} />
         <Route path="/regist" element={<Regist />} />
+        <Route path="/destinacio" element={<Destinacio />} />
+        <Route path="/perfil" element={<Perfil />} />
         <Route path="*" element={<h1>404</h1>} />
       </Routes>
     </BrowserRouter>
   </StrictMode>
-)
+);
